8a9669b8a3751eff9ac7c0a85d4820486e84a176
BrunodevOliveira <brunodevoliveira@gmail.com>
2022-09-13 09:53:38 -0300

    add ClientController

---
+++ src/main/java/com/api/crudPessoa/controller/ClientController.java
@@ -0,0 +1,63 @@
+package com.api.crudPessoa.controller;
+
+import java.util.List;
+
+import com.api.crudPessoa.model.Cliente;
+import com.api.crudPessoa.servico.Servico;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.api.crudPessoa.model.Pessoa;
+import com.api.crudPessoa.repositories.PersonRepository;
+
+import javax.validation.Valid;
+
+import javax.validation.Valid;
+
+@RestController
+public class ClientController {
+    @Autowired
+    private PersonRepository action;
+
+    @Autowired
+    private Servico service;
+
+    @PostMapping("/api")
+    public ResponseEntity<?> signUp(@RequestBody Pessoa pessoa) {
+        return service.signUp(pessoa);
+    }
+
+    @GetMapping("/api")
+    public ResponseEntity<?> selectAll() {
+        return service.selectAll();
+    }
+
+    @GetMapping("/api/{id}")
+    public ResponseEntity<?> selectById(@PathVariable int id) {
+        return service.selectById(id);
+    }
+
+    @GetMapping("usuario/{name}")
+    public Pessoa selectByName(@PathVariable String name) {
+        return action.findByName(name);
+    }
+
+    @PutMapping("/api")
+    public ResponseEntity<?> editPerson(@RequestBody Pessoa obj) {
+        return service.editPerson(obj);
+    }
+
+    @DeleteMapping("/api/{id}")
+    public ResponseEntity<?> removePerson(@PathVariable int id) { return service.removePerson(id); }
+
+    @PostMapping("/cliente")
+    public void Client(@Valid @RequestBody Cliente obj){}
+}
